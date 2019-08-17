5500bc0ec6977b79e1adb17148cd5245c5afce99
Robert Cortez <Nuhuhfoo@gmail.com>
2019-08-16 22:24:49 -0700

    added faq menu

--- rkeeppup/src/components/dashboard/dashboard.js
+++ rkeeppup/src/components/dashboard/dashboard.js
@@ -1,14 +1,31 @@
 import React, { Component } from "react";
 import './dashboard.css';
 import Faq from '../../images/faq.png';
+import Close from '../../images/close.png';
 
 class Dashboard extends Component {
     render () {
+        function displayNav() {
+            var nav = document.getElementById("myNav");
+            nav.style.width = nav.style.width === '100%' ? '0%' : '100%';
+        }
         return (
             <div>
                 <div>
-                <img className="faq" src={Faq} alt="faq"/>
+                    <img onClick={displayNav} className="faq" src={Faq} alt="faq"/>
                 </div>
+                
+                <div id="myNav" className="overlay">
+                    <div onClick={displayNav} className="closebtn">
+                        <img src={Close} alt="close"/>
+                    </div>
+                    <div className="overlay-content">
+                        <div className="instructions">
+                            <p>Welcome to your Dashboard</p>
+                        </div>
+                    </div>
+                </div>
+
                 <div>
                     <p className="dashboard">My</p>
                     <p className="dashboard2">Dashboard</p>
