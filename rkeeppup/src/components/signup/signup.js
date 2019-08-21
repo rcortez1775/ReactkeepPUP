91e06508a1cdb8ecd7e25ba771681af99e65aa7c
Robert Cortez <Nuhuhfoo@gmail.com>
2019-08-20 17:08:36 -0700

    updated sign up view

--- rkeeppup/src/components/signup/signup.js
+++ rkeeppup/src/components/signup/signup.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import './signup.css';
 import Kennel from '../../images/dog-kennel.png';
+import Family from '../../images/pet.png'
 import {Link} from 'react-router-dom';
 
 class Signup extends Component {
@@ -12,10 +13,26 @@ class Signup extends Component {
                         <img className="previous" src={Kennel} alt="Kennel"/>
                     </Link> 
                 </div> 
+                <div className="biscuitLogo">
+                    <img className="pup" src={Family} alt="Family"/>
+                </div>  
                 <div>
                     <p className="family">Join the Family!</p>
                 </div>
 
+                <div className="signUp">
+                    <form>
+                        <label for="firstName">First Name:</label>
+                        <input type="text" id="firstName" name="firstname"/>
+                        <label for="lastName">Last Name:</label>
+                        <input type="text" id="lastName" name="lastname"/>
+                        <label for="PetName">Pet's Name:</label>
+                        <input type="text" id="petName" name="petname"/>
+                    </form>
+                    <div className="buttonCont">
+                            <button type="submit" className="createBtn">&#43; Create Account</button>
+                    </div> 
+                </div>
             
             </div>
         )
