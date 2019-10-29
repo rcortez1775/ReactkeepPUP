83001cb696b4fb7931f7c558feedc4310e5b454c
Robert Cortez <Nuhuhfoo@gmail.com>
2019-10-28 19:43:25 -0700

    updated home page

--- rkeeppup/src/components/landingpage/landingpage.js
+++ rkeeppup/src/components/landingpage/landingpage.js
@@ -1,7 +1,7 @@
 import { Component } from 'react';
 import React from 'react';
 import './landingpage.css';
-import Logo from '../../images/dog.png';
+import Logo from '../../images/dogscene.jpg';
 import {Link} from 'react-router-dom';
 
 
@@ -10,15 +10,17 @@ class LandingPage extends Component {
         return (
             <div>
                 <div className="titleContainer">
-                    <p className="title">keep[PUP]</p>
+                    <p className="title">keepPUP</p>
                     <p className="slogan">Store. Track. Share.</p>
                 </div>
-                <div className="logoCont">
-                    <img className="logo bounce" src={Logo} alt="Logo"/>
+                <div className="signUpCont">
+                    <Link to="/signup"><button className="button">Get Started</button></Link>
                 </div>
-                <div className="buttonCont">
-                    <Link to="/loginpage"><button className="button">Login</button></Link>
-                    <Link to="/signup"><button className="button">Sign Up</button></Link>
+                <div className="logoCont">
+                    <img className="logo" src={Logo} alt="Logo"/>
+                </div>
+                <div className="loginCont">
+                    <p className="loginBtn">Already a member? <Link to="/loginpage"><button className="loginBtn">Login</button></Link></p>
                 </div>
             </div>
         )
