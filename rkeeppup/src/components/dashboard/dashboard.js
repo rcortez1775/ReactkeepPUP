1fdf0c1a385ee6c75213a18b093b016ca61691a5
Robert Cortez <Nuhuhfoo@gmail.com>
2019-10-30 22:53:22 -0700

    updated page

--- rkeeppup/src/components/dashboard/dashboard.js
+++ rkeeppup/src/components/dashboard/dashboard.js
@@ -3,6 +3,8 @@ import './dashboard.css';
 import Faq from '../../images/question.png';
 import Close from '../../images/close.png';
 import Add from '../../images/whiteadd.png';
+import AddPet from '../../images/addPet.png';
+import Plus from '../../images/plus.png';
 
 class Dashboard extends Component {
     render () {
@@ -15,7 +17,6 @@ class Dashboard extends Component {
                 <div className="faqCont">
                     <img onClick={displayNav} className="faq" src={Faq} alt="faq"/>
                 </div>
-                
                 <div id="myNav" className="overlay">
                     <div onClick={displayNav} className="closebtn">
                         <img src={Close} alt="close"/>
@@ -27,13 +28,19 @@ class Dashboard extends Component {
                         </div>
                     </div>
                 </div>
-
                 <div className="dashboardCont">
                     <p className="dashboard">My</p>
                     <p className="dashboard2">Dashboard</p>
                 </div>
                 <div>
                     <p className="pets">My Pets</p>
+                    <img src={AddPet} className="addPet" alt="add"/>
+                </div>
+                <div className="petCont">
+                    <form className="pure-form">
+                        <p className="pupTracker">Pup Tracker</p>
+                    </form>
+                    <img src={Plus} className="activityBtn" alt="plus"/>
                 </div>
             </div>
         )
