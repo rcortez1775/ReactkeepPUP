a9ea65f9648d0276b540a680d7d08be936ac16de
Robert Cortez <Nuhuhfoo@gmail.com>
2019-11-14 18:13:47 -0800

    updated acitivty buttons

--- rkeeppup/src/components/dashboard/dashboard.js
+++ rkeeppup/src/components/dashboard/dashboard.js
@@ -5,12 +5,22 @@ import Close from '../../images/close.png';
 import Add from '../../images/whiteadd.png';
 import AddPet from '../../images/addPet.png';
 import Plus from '../../images/plus.png';
+import Food from '../../images/petfood.png';
+import Walk from '../../images/walk.png';
+import Medicine from '../../images/medicine.png';
+import Bath from '../../images/bath.png';
 
 class Dashboard extends Component {
     render () {
+        //This is the function that displays the menu
         function displayNav() {
             var nav = document.getElementById("myNav");
             nav.style.width = nav.style.width === '100%' ? '0%' : '100%';
+        }
+        //This is the function that displays the activity buttons
+        function myFunction() {
+            var x = document.getElementById("buttons");
+            x.style.display = x.style.display === 'none' ? 'block' : 'none';
         }
         return (
             <div className="yellow">
@@ -40,7 +50,13 @@ class Dashboard extends Component {
                     <form className="pure-form">
                         <p className="pupTracker">Pup Tracker</p>
                     </form>
-                    <img src={Plus} className="activityBtn" alt="plus"/>
+                    <div id="buttons">
+                        <div className="key"><img src={Medicine} className="icon" alt="medicine"/></div>
+                        <div className="key"><img src={Walk} className="icon" alt="walk"/></div>
+                        <div className="key"><img src={Food} className="icon" alt="food"/></div>
+                        <div className="key"><img src={Bath} className="icon" alt="bath"/></div>
+                    </div>
+                    <img onClick={myFunction} src={Plus} className="activityBtn" alt="plus"/>
                 </div>
             </div>
         )
